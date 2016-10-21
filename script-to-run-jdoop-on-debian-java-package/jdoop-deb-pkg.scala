808d98662d146666f51496e8f36fbfc42e7aad41
Marko Dimjašević <marko@cs.utah.edu>
2016-10-21 04:48:33 +0200

    Made java src dir and FQDN guessing more robust, though not perfect

--- script-to-run-jdoop-on-debian-java-package/jdoop-deb-pkg.scala
+++ script-to-run-jdoop-on-debian-java-package/jdoop-deb-pkg.scala
@@ -55,28 +55,6 @@ object runJDoop {
       .flatMap(recursiveListFiles(_, r)))
   }
 
-  /**
-    * Determines the root of .java files for an unpacked Debian Java
-    * source package
-    */
-  def javaSrcDir(srcPkgName: String): File = {
-    val dir = getSrcPkgDir(srcPkgName)
-    val javaFiles = recursiveListFiles(new File(dir),
-      """.*\.java""".r)
-      .map { _.toString.substring((dir + File.separator).length) }
-    val srcDirMap = javaFiles groupBy { f =>
-      f.substring(0, f.indexOf(File.separator) max 0) }
-    val fileCountMap = srcDirMap map { kv => (kv._1, kv._2.length) }
-    val mainSrcDir = fileCountMap.toSeq.sortBy(_._2).last._1
-    Seq("main" + File.separator + "java", "java", "").map { d: String =>
-      val nd: String = mainSrcDir + File.separator + d
-      (nd, (new File(dir + File.separator + nd)).exists)
-    }
-      .filter { _._2 }
-      .map { p => new File(p._1) }
-      .apply(0)
-  }
-
   def createTmpDir(): Path = Files.createTempDirectory("jdoop-")
 
   def installBuildDeps(srcPkgName: String): Unit =
@@ -189,6 +167,36 @@ object runJDoop {
       .sortBy(-_.length)
   }
 
+  /** Guesses a FQDN of a project based on a directory with most Java
+    * (bytecode) files.
+    *
+    * @param files A sequence of files to base the decision on
+    * @return A fully qualified domain name
+    */
+  def guessFQDNFromDirStruct(files: Seq[String]): String = {
+    val s = Stream.from(1).map { groupByPkg(files, _) }
+    val rootPkgs = s(0)
+
+    rootPkgs.length match {
+      // Either all classes are in the same root package so find the
+      // longest common prefix...
+      case 1 =>
+        val sWithIndices = s zipWithIndex
+        val commonPrefix = sWithIndices takeWhile{ pkgs =>
+          pkgs._1.length == 1 &&
+          (pkgs._2 match {
+            case 0 => true
+            case i => sWithIndices(i - 1)._1 != pkgs._1
+          })
+        }
+        commonPrefix.last._1(0)(0)
+      // ... or there is more than one root package so return one
+      // with the most classes in it. All packages not returned here
+      // will not be tested by JDoop.
+      case _ => rootPkgs(0)(0)
+    }
+  }
+
   /**
     * Gets the fully qualified domain name of the main Java package in
     * a given JAR file
@@ -197,73 +205,171 @@ object runJDoop {
     * @return The fully qualified name of the Java package
     */
   def getJavaPackageName(jar: File): String = {
-    def guessFromDirStruct: String = {
-      val classFiles = getJarEntries(jar).filter {
-        _.getName.endsWith(".class") }
-        .map { _.toString }
-
-      val s = Stream.from(1).map { groupByPkg(classFiles, _) }
-      val rootPkgs = s(0)
-
-      rootPkgs.length match {
-        // Either all classes are in the same root package so find the
-        // longest common prefix...
-        case 1 =>
-          val sWithIndices = s zipWithIndex
-          val commonPrefix = sWithIndices takeWhile{ pkgs =>
-            pkgs._1.length == 1 &&
-            (pkgs._2 match {
-              case 0 => true
-              case i => sWithIndices(i - 1)._1 != pkgs._1
-            })
-          }
-          commonPrefix.last._1(0)(0)
-        // ... or there is more than one root package so return one
-        // with the most classes in it. All packages not returned here
-        // will not be tested by JDoop.
-        case n => rootPkgs(0)(0)
-      }
-    }
 
     val manifest = new JarFile(jar).getManifest
     // an immutable map; it would have been a mutable one without .toMap
     val entryMap = manifest.getEntries.asScala.toMap
     entryMap.keySet.toSeq match {
-      case Seq() => guessFromDirStruct
+      case Seq() =>
+        val classFiles = getJarEntries(jar).filter {
+          _.getName.endsWith(".class") }
+          .map { _.toString }
+        guessFQDNFromDirStruct(classFiles)
       case seq => seq.sortBy(_.length).apply(0).split('/').mkString(".")
     }
   }
 
-  def main(args: Array[String]) {
-    // val testPkgs = List("libxom-java", "libglazedlists-java", "libmapscript-java")
-    val testPkgs = Seq(
-      // "libswingx1-java",
-      "libswt-gtk-3-java")// ,
-      // "libtomcat7-java",
-      // "libtrilead-ssh2-java",
-      // "libvecmath-java",
-      // "libvldocking-java",
-      // "libwagon-java",
-      // "libwagon2-java",
-      // "libxalan2-java")
+  /**
+    * Returns a pair representing a Java source directory and the
+    * fully qualified domain name of the Java project in that source
+    * directory.
+    */
+  def getSourceInfo(srcPkgName: String): (String, String) = {
+    // A list of common directory structures for a source code tree
+    val seq = List(
+      List("src", "main", "java"),
+      List("src", "java", "main"),
+      List("src", "java"),
+      List("main", "java"),
+      List("main"),
+      List("java"),
+      List("src"),
+      List("")
+    )
+    /**
+      * Finds the longest common directory structure for Java projects
+      */
+    def longestDirPrefix(topDir: String, srcDir: String): File = {
+      seq.map { d: List[String] =>
+        val nd: String =
+          srcDir + File.separator + d.mkString(File.separator)
+        (nd, (new File(topDir + File.separator + nd)).exists)
+      }
+        .filter { _._2 }
+        .map { p => new File(p._1) }
+        .apply(0)
+    }
 
-    for (binPkgName <- testPkgs) {
+    /**
+      * Determines the root of .java files for an unpacked Debian Java
+      * source package
+      */
+    def getJavaSrcDir: File = {
+      /**
+        * Determines the best source directory candidate starting at a
+        * given directory.
+        *
+        * @param dir The root directory where to search
+        * @return The best candidate for the source directory
+        */
+      def bestDirCandidate(dir: String): String = {
+        val javaFiles = recursiveListFiles(new File(dir),
+          """.*\.java""".r)
+          .map { _.toString.substring((dir + File.separator).length) }
+        val srcDirMap = javaFiles groupBy { f =>
+          f.substring(0, f.indexOf(File.separator) max 0) }
+        val fileCountMap = srcDirMap map { kv => (kv._1, kv._2.length) }
+        fileCountMap.toSeq.sortBy(_._2).last._1
+      }
+
+      val dir = getSrcPkgDir(srcPkgName)
+      val mainSrcDir: String = bestDirCandidate(dir)
+      val candidateResult: File = longestDirPrefix(dir, mainSrcDir)
+      // make sure we are not getting into an empty directory
+      if (recursiveListFiles(
+        new File(dir + File.separator + candidateResult.toString),
+        """.*\.java""".r)
+        .size == 0)
+        new File(mainSrcDir)
+      else
+        candidateResult
+    }
+
+    /**
+      * Gets the fully qualified domain name of the main Java package in
+      * a given source package
+      *
+      * @param mainSrcDir A source directory structure before the fully
+      *        qualified domain name
+      * @return The fully qualified name of the Java package
+      */
+    def getJavaPackageName(mainSrcDir: String): String = {
+      val srcPkgDir  = getSrcPkgDir(srcPkgName)
+      val dir = srcPkgDir + File.separator + mainSrcDir
+      val javaFiles =
+        recursiveListFiles(new File(dir), """.*\.java""".r)
+          .map { _.toString.substring((dir + File.separator).length) }
+      guessFQDNFromDirStruct(javaFiles)
+    }
+
+    def extendWithSneaky(tuple: (String, String),
+      sneaky: List[String]): (String, String) = {
+
+      val (javaDir, fqdn) = tuple
+      val fqdnList = fqdn.split('.').toList
+      if (fqdnList.startsWith(sneaky))
+        (javaDir + File.separator + sneaky.mkString(File.separator),
+          fqdnList.drop(sneaky.length).mkString("."))
+      else
+        (javaDir, fqdn)
+    }
+
+    val javaDirLvl1 = getJavaSrcDir.toString
+    val fqdnLvl1    = getJavaPackageName(javaDirLvl1)
+
+    val (javaDirLvl2, fqdnLvl2) =
+      if (fqdnLvl1.isEmpty)
+        // there is a one-level hierarchy and that's at the same time
+        // the package name
+        (".", javaDirLvl1)
+      else
+        // there is at least one directory level before the fully
+        // qualified domain name directory structure begins
+        (javaDirLvl1, fqdnLvl1)
+
+    // check if any usual directory levels are still in the FQDN part
+    val (javaDirLvl3, _) = seq.dropRight(1) // drop the empty string at the end
+      .foldLeft(
+      (javaDirLvl2, fqdnLvl2)){
+      (tuple, sneakyFqdn) => extendWithSneaky(tuple, sneakyFqdn)}
+
+    (javaDirLvl3, getJavaPackageName(javaDirLvl3))
+  }
+
+  def main(args: Array[String]) {
+    val source = scala.io.Source.fromFile(args(0))
+    val testPkgs = try source.mkString.split("\n").toSeq finally source.close()
+
+    // a new flow based on starting with source packages
+    //
+    // For a given source package named srcPkgName, do:
+    //
+    // - downloadSrcPkg(srcPkgName)
+    // - install its build dependencies: sudo apt-get build-dep <srcPkgName>
+    // - build the source package: DEB_BUILD_OPTIONS=nocheck fakeroot debian/rules build
+    // - install all binary packages it builds, just in case (get that
+    //   info from its .dsc file)
+    // - figure out where are .class files
+    // - figure out the java package name just like with getJavaPackageName
+    for (srcPkgName <- testPkgs) {
       println("======================================")
-      println("Binary package: " + binPkgName)
-      val srcPkgName = getSrcPkgName(binPkgName)
       println("Source package: " + srcPkgName)
       downloadSrcPkg(srcPkgName)
-      println(javaSrcDir(srcPkgName))
-      installBinPkg(binPkgName)
-      val tmpDir = createTmpDir()
-      println(tmpDir)
-      val jars = getBinPkgJars(binPkgName)
-      println("This many jars: " + jars.size)
-      if (jars.size > 0) {
-        unpackJars(getBinPkgJars(binPkgName), tmpDir)
-        println("Java package name: " +
-          getJavaPackageName(getBinPkgJars(binPkgName).toSeq.apply(0)))
-      }
+      // installBuildDeps(srcPkgName)
+      val (javaDir, fqdn) = getSourceInfo(srcPkgName)
+      println("Java src dir = " + javaDir)
+      println("FQDN         = " + fqdn)
+
+      // installBinPkg(binPkgName)
+      // val tmpDir = createTmpDir()
+      // println(tmpDir)
+      // val jars = getBinPkgJars(binPkgName)
+      // println("This many jars: " + jars.size)
+      // if (jars.size > 0) {
+      //   unpackJars(getBinPkgJars(binPkgName), tmpDir)
+      //   println("Java package name: " +
+      //     getJavaPackageName(getBinPkgJars(binPkgName).toSeq.apply(0)))
+      // }
     }
   }
 }
