222e9ecbaf12932a699737054f8c964a005e1ffa
Marko Dimjašević <marko@cs.utah.edu>
2016-11-21 23:36:47 -0700

    Wrote a script that prepares an Apache Spark worker node

---
+++ spark/prepare-worker-node.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Prepares a worker (slave) node for computation
+
+set -e
+
+PKGS="lxc scala"
+sudo apt-get update && sudo apt-get install --yes $PKGS
+
+WRAPPER_HOME="$(cd "`dirname "$0"`"/..; pwd)"
+
+$WRAPPER_HOME/lxc/create-debian-container.scala
+$WRAPPER_HOME/lxc/create-jdoop-container.scala stretch jdoop
+# WRAPPER_HOME/nfs/install-nfs-client.scala ${NFS_SERVER} ${SERVER_DIR} ${MOUNT_DIR}
+$WRAPPER_HOME/spark/install-spark.sh
+$WRAPPER_HOME/cpu/disable-hyperthreading.sh || true
+
+. ${WRAPPER_HOME}/spark/my-env.sh
+cp ${WRAPPER_HOME}/spark/conf/spark-env-slave.sh ${SPARK_HOME}/conf/spark-env.sh
