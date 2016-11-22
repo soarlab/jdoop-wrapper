b28015fbaaad36ad6a21e556b498e620756a7c4a
Marko Dimjašević <marko@cs.utah.edu>
2016-11-22 15:06:48 -0700

    NFS: updated prepare-scripts for Spark master and workers

--- spark/prepare-worker-node.sh
+++ spark/prepare-worker-node.sh
@@ -8,10 +8,11 @@ PKGS="lxc scala"
 sudo apt-get update && sudo apt-get install --yes $PKGS
 
 WRAPPER_HOME="$(cd "`dirname "$0"`"/..; pwd)"
+. ${WRAPPER_HOME}/spark/my-env.sh
 
 $WRAPPER_HOME/lxc/create-debian-container.scala
 $WRAPPER_HOME/lxc/create-jdoop-container.scala stretch jdoop
-# WRAPPER_HOME/nfs/install-nfs-client.scala ${NFS_SERVER} ${SERVER_DIR} ${MOUNT_DIR}
+$WRAPPER_HOME/nfs/install-nfs-client.scala ${NFS_SERVER} ${SERVER_DIR} ${MOUNT_DIR}
 $WRAPPER_HOME/spark/install-spark.sh
 $WRAPPER_HOME/cpu/disable-hyperthreading.sh || true
 
