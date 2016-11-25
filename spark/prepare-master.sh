eee5231c9733e87627eaf9d212256d7a4612f684
Marko Dimjašević <marko@cs.utah.edu>
2016-11-24 18:55:56 -0700

    Added SBT instalation to the prepare master script

--- spark/prepare-master.sh
+++ spark/prepare-master.sh
@@ -4,5 +4,6 @@
 
 WRAPPER_HOME="$(cd "`dirname "$0"`"/..; pwd)"
 ${WRAPPER_HOME}/spark/install-spark.sh
+${WRAPPER_HOME}/spark/install-sbt.scala
 . ${WRAPPER_HOME}/spark/my-env.sh
 ${WRAPPER_HOME}/nfs/install-nfs-server.scala ${NFS_NETWORK} ${SERVER_DIR}
