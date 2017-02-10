ed3390212702217b6987efc8817a70c09145c3f0
Marko Dimjašević <marko@cs.utah.edu>
2017-02-09 19:17:39 -0700

    Added a copyright header

--- cpu/disable-hyperthreading.sh
+++ cpu/disable-hyperthreading.sh
@@ -1,8 +1,28 @@
 #!/usr/bin/env bash
 
-# Disable hyperthreading on CPUs
+# Copyright 2017 Marko Dimjašević
+#
+# This file is part of jdoop-wrapper.
+#
+# jdoop-wrapper is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# jdoop-wrapper is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with maline.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# Disable hyperthreading on CPUs. Taken from
+# https://serverfault.com/a/792264.
+
 cat /sys/devices/system/cpu/cpu*/topology/thread_siblings_list | \
 awk -F, '{print $2}' | \
 sort -n | \
 uniq | \
-( while read X ; do echo $X ; echo 0 | sudo tee /sys/devices/system/cpu/cpu$X/online ; done )
+( while read X; do echo $X; echo 0 | sudo tee /sys/devices/system/cpu/cpu$X/online; done )
--- spark/my-env.sh
+++ spark/my-env.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+# Copyright 2017 Marko Dimjašević
+#
+# This file is part of jdoop-wrapper.
+#
+# jdoop-wrapper is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# jdoop-wrapper is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with maline.  If not, see <http://www.gnu.org/licenses/>.
+
+
 SPARK_MASTER_HOST=${SPARK_MASTER_HOST:-node-1.multinode.jpf-doop.emulab.net}
 
 SPARK_DIR=/mnt/storage/spark
