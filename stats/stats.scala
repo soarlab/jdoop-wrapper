cf9dc9ea16af517b6f767d3f585090272f9bd102
Marko Dimjašević <marko@cs.utah.edu>
2017-02-14 10:34:28 -0700

    Removed a needless conversion to double

--- stats/stats.scala
+++ stats/stats.scala
@@ -49,10 +49,8 @@ object Stats {
     lazy val ratioAvg: Double = if (total == 0) 0.0 else coveredAvg / total
     lazy val percentageAvg: Double = 100.0 * ratioAvg
     lazy val stdDev: Double = Math.sqrt(
-      (covered.map{ c => (c - coveredAvg) * (c - coveredAvg) }
-        .sum
-        .toDouble
-      ) / covered.length
+      covered.map{ c => (c - coveredAvg) * (c - coveredAvg) }.sum /
+        covered.length
     )
 
     override def toString: String = Seq(
